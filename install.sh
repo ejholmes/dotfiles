a3e7fb078c8128d90f75b7307b354e8939622696
Eric J. Holmes <eric@ejholmes.net>
2012-06-30 21:51:00 -0700

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "initializing submodules"
+git submodule init
+git submodule update
+
+echo "Deleting the old files"
+# rm ~/.vimrc
+# rm ~/.zshrc
+# rm ~/.vim
+# rm ~/.gvimrc
+# rm ~/.gitconfig
+rm ~/.gitignore
+# rm ~/.tmux.conf
+
+echo "Symlinking files"
+# ln -s ~/Source/dotfiles/vimrc ~/.vimrc
+# ln -s ~/Source/dotfiles/zshrc ~/.zshrc
+# ln -s ~/Source/dotfiles/vim ~/.vim
+# ln -s ~/Source/dotfiles/gvimrc ~/.gvimrc
+# ln -s ~/Source/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/Source/dotfiles/gitignore ~/.gitignore
+# ln -s ~/Source/dotfiles/tmux ~/.tmux.conf
+
+echo "Updating submodules"
+git submodule foreach git pull origin master --recurse-submodules
+
+echo "All done."
